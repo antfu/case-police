fca347cb376f58597460d74de1f2ea1969cfbf3c
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-31 18:32:42 +0800

    feat: update dict

--- src/dict.ts
+++ src/dict.ts
@@ -2,10 +2,15 @@
 
 export const dictionary: Record<string, string> = {
   cjs: 'CJS',
+  css: 'CSS',
   eslint: 'ESLint',
   esm: 'ESM',
   github: 'GitHub',
+  html: 'HTML',
+  ios: 'iOS',
+  iphone: 'iPhone',
   javascript: 'JavaScript',
+  macos: 'macOS',
   typescript: 'TypeScript',
   vscode: 'VS Code',
   vueuse: 'VueUse',
