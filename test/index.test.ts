d7f26e88acce740f265fdda50bd5b0117d57c88b
Xwil <xw5427@gmail.com>
2022-04-02 00:29:13 +0800

    test: add test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { replace } from '../src/utils'
+import { replace, resolvePreset } from '../src/utils'
 
 /** @case-police-ignore */
 
@@ -19,5 +19,23 @@ vscode VScode VS Code VSCODE vs code VS code
   vscode VS Code VS Code VSCODE vs code VS Code
   "
 `)
+  })
+})
+
+describe('presets', () => {
+  it('works', async() => {
+    const applePreset = await resolvePreset('apple')
+
+    const replaced = await replace(
+      `
+      macOs Macbook
+    `,
+      '',
+      applePreset,
+    )
+
+    expect(replaced).toBe(`
+      macOS MacBook
+    `)
   })
 })
