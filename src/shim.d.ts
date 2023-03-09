d5ed5d9448378397ce8a93f86358c6823b8b7c9c
ntnyq <ntnyq13@gmail.com>
2023-03-09 17:26:02 +0800

    fix: fix parse gitignore (#149)

---
+++ src/shim.d.ts
@@ -0,0 +1,27 @@
+
+declare module 'parse-gitignore' {
+  interface Section {
+    comment?: string
+    patterns?: string[]
+  }
+
+  interface ParsedResult {
+    patterns: string[]
+    sections: Section[]
+    path?: string
+    input: Buffer
+  }
+
+  interface ParseOptions {
+    path?: string
+    dedupe?: boolean
+    unique?: boolean
+    ignore?: string[]
+    unignore?: string[]
+    formatSection? (section?: Section): string
+  }
+
+  declare function parseGitignore(input: string, options?: ParseOptions): ParsedResult
+
+  export = parseGitignore
+}
