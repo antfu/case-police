ed78ebcc1f1f6c37f359f03c9f201b39fc4f1b23
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-09 10:33:43 +0100

    test: duplicate test

---
+++ test/duplicate.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest'
+import fg from 'fast-glob'
+import { loadAllPresets, resolvePreset } from '../src/utils'
+
+describe('duplicate', () => {
+  it('detection', async () => {
+    const names = (await fg('dict/*.json')).map(name => name.replace('dict/', '').replace('.json', ''))
+    const presets = await Promise.all(names.map(async name => ({
+      name,
+      preset: await resolvePreset(name) as Record<string, string>,
+    })))
+
+    const allPresets = await loadAllPresets()
+
+    const duplicates: any[] = []
+
+    Object.keys(allPresets).forEach((value, key) => {
+      const matched = presets.filter(preset => preset.preset[value])
+      if (matched.length > 1) {
+        duplicates.push({
+          value,
+          matched: matched.map(p => p.name),
+        })
+      }
+    })
+
+    expect(duplicates).toEqual([])
+  })
+})
