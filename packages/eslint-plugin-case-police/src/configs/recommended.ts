1a3ff289f6d08554528d540961cbd679db6ca4b9
Oliver Mak <oliver.mak@naxos.com>
2025-03-21 14:43:07 +0800

    Divide into setup and rules

--- packages/eslint-plugin-case-police/src/configs/recommended.ts
+++ packages/eslint-plugin-case-police/src/configs/recommended.ts
@@ -1,14 +1,19 @@
 import pluginCasePolice from '../index'
 
-export default {
-  name: 'case-police',
-  files: ['**/*.?([cm])[jt]s?(x)'],
-  plugins: {
-    get 'case-police'(): any {
-      return pluginCasePolice
+export default [
+  {
+    name: 'case-police/setup',
+    plugins: {
+      get 'case-police'(): any {
+        return pluginCasePolice
+      },
     },
   },
-  rules: {
-    'case-police/string-check': 'warn',
+  {
+    name: 'case-police/rules',
+    files: ['**/*.?([cm])[jt]s?(x)'],
+    rules: {
+      'case-police/string-check': 'warn',
+    },
   },
-}
+]
